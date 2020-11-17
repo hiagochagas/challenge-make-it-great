9561f327a62ed1f77db21f42e2e70d26d42c0929
Tales Conrado <talesmateuscc@gmail.com>
2020-11-17 11:29:35 -0300

    fixes comment in view code protocol

--- MakeItGreat/Protocols/ViewCode.swift
+++ MakeItGreat/Protocols/ViewCode.swift
@@ -7,15 +7,18 @@
 
 import Foundation
 
-/// Assine este protocolo para implementar view code em sua controller.
+/// Assine este protocolo para implementar view code em sua view.
 /// 
 /// Exemplo de uso:
 /// ```
-/// class HomeController: UIViewController, ViewCode
+/// class HomeView: UIView, ViewCode
 /// ```
 protocol ViewCode {
+    /// Use esse método para adicionar subviews.
     func setViewHierarchy()
+    /// Use esse método para organizar suas constraints. Separe em subfunções se necessário.
     func setConstraints()
+    /// Função que chama os outros métodos do protocolo. Chame-a no seu init, por exemplo.
     func setup()
 }
 
