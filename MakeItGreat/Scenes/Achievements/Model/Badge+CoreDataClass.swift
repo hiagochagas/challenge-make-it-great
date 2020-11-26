e1227bcca449d35133980bfcc726a2499a62ef01
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-11-26 11:57:10 -0300

    adding new files

---
+++ MakeItGreat/Scenes/Achievements/Model/Badge+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Badge+CoreDataClass.swift
+//  MakeItGreat
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 26/11/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Badge)
+public class Badge: NSManagedObject {
+
+}
---
+++ MakeItGreatTests/AchievementsViewModelTest.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  MakeItGreatTests
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 26/11/20.
+//
+
+import Foundation
+import CoreData
+@testable import MakeItGreat
+
+class AchievementsViewModelTest {
+     AchievementsViewModel
+    init(){
+        
+    }
+    
+    func create_badge_test(){
+        
+    }
+}
