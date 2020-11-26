726c35373f679be53ce53df5cca1ff187216cc75
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-11-26 11:59:15 -0300

    adding badge model

--- MakeItGreat/Scenes/Achievements/Model/Badge+CoreDataClass.swift
+++ MakeItGreat/Scenes/Achievements/Model/Badge+CoreDataClass.swift
@@ -11,5 +11,13 @@ import CoreData
 
 @objc(Badge)
 public class Badge: NSManagedObject {
-
+    
+    static func fetchAllBadges(viewContext: NSManagedObjectContext = AppDelegate.viewContext) -> [Badge] {
+        let request : NSFetchRequest<Badge> = Badge.fetchRequest()
+            request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        guard let badges = try? viewContext.fetch(request) else {
+            return []
+        }
+        return badges
+    }
 }
