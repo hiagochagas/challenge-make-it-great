87e70946e4b86594cef260b3b8881a10b2b3e07a
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-11-26 12:00:44 -0300

    creating core data functions to badge model

--- MakeItGreat/Scenes/Achievements/ViewModel/AchievementsViewModel.swift
+++ MakeItGreat/Scenes/Achievements/ViewModel/AchievementsViewModel.swift
@@ -2,7 +2,67 @@
 //  AchievementsViewModel.swift
 //  MakeItGreat
 //
-//  Created by Tales Conrado on 20/11/20.
 //
 
 import Foundation
+import CoreData
+import UIKit
+
+class AchievementsViewModel {
+    
+    var badges: [Badge]?
+    
+    init(context: NSManagedObjectContext){
+        badges = fetchBadges(context: context)
+    }
+    
+    func createBadge(id: UUID = UUID(), descriptionBadge: String, icon: Data, name: String, progress: Int64, viewContext: NSManagedObjectContext) -> Badge? {
+        guard let badge = NSEntityDescription.insertNewObject(forEntityName: "Badge", into: viewContext) as? Badge else {return nil}
+        badge.id = id
+        badge.name = name
+        badge.icon = icon
+        badge.progress = progress
+        badge.descriptionBadge = descriptionBadge
+        save(context: viewContext)
+        
+        return badge
+    }
+    
+    func updateBadge(badge: Badge, name: String, icon: Data, descriptionBadge: String, progress: Int64, viewContext: NSManagedObjectContext) -> Badge? {
+        guard let badge = NSEntityDescription.insertNewObject(forEntityName: "Badge", into: viewContext) as? Badge else {return nil}
+        badge.progress = progress
+        badge.icon = icon
+        badge.descriptionBadge = descriptionBadge
+        badge.name = name
+        save(context: viewContext)
+        return badge
+    }
+    
+    func updateBadgeProgress(badge: Badge, progress: Int64, viewContext: NSManagedObjectContext) -> Badge? {
+        guard let badge = NSEntityDescription.insertNewObject(forEntityName: "Badge", into: viewContext) as? Badge else {return nil}
+        badge.progress = progress
+        save(context: viewContext)
+        return badge
+    }
+    
+    func fetchBadges(context: NSManagedObjectContext) -> [Badge]{
+        let badges = Badge.fetchAllBadges(viewContext: context)
+        return badges
+    }
+    
+    func deleteBadge(badge: Badge, context: NSManagedObjectContext){
+        context.delete(badge)
+        self.badges = fetchBadges(context: context)
+        save(context: context)
+    }
+    
+    func save(context: NSManagedObjectContext) {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print("Save error \(error)")
+            }
+        }
+    }
+}
