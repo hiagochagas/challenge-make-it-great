12d9a6b8323e84661920bd398af0d2c65a0bc0db
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-12-02 11:38:00 -0300

    persisting changes to coredata and ccreating logic for checking priority

--- MakeItGreat/Scenes/BottomSheet/Controllers/BottomSheetViewController.swift
+++ MakeItGreat/Scenes/BottomSheet/Controllers/BottomSheetViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class BottomSheetViewController: UIViewController {
     let lists = ["Next", "Inbox", "Waiting", "Projects", "Maybe"]
+    let homeModel = HomeViewModel()
     var task: Task?
+    var delegate: ModalHandler?
     var contentView: UIView = UIView() // trocar pela view especifica depois
     var viewModel: BottomSheetViewModel?
-    weak var homeCoordinator: HomeCoordinator?
+    var taskNameTextField: UITextField?
+    var tagTextField: UITextField?
+    var priority: Int?
+    var greenButton: UIButton?
+    var yellowButton: UIButton?
+    var redButton: UIButton?
+    
+    //    let homeController = HomeViewController(viewModel: HomeViewModel())
+    //    weak var homeCoordinator: HomeCoordinator?
     
     
     //    init(viewModel: BottomSheetViewModel, homeCoordinator: HomeCoordinator, view: BottomSheetView) {
@@ -29,32 +39,102 @@ class BottomSheetViewController: UIViewController {
     override func loadView() {
         let bottomView = BottomSheetView()
         bottomView.viewController = BottomSheetViewController()
-        bottomView.saveButton.target(forAction: #selector(saveButton), withSender: self)
+        bottomView.saveButton.addTarget(self, action: #selector(saveButton(_:)), for: .touchUpInside)
+        bottomView.cancelButton.addTarget(self, action: #selector(cancelButton(_:)), for: .touchUpInside)
+        taskNameTextField = bottomView.textFieldTaskTitle
+        taskNameTextField?.text = task?.name
+        tagTextField = bottomView.textFieldTag
+        tagTextField?.text = task?.tags
+        greenButton = bottomView.greenButton
+        redButton = bottomView.redButton
+        yellowButton = bottomView.yellowButton
+        greenButton?.addTarget(self, action: #selector(checkPriorityButton(_:)), for: .touchUpInside)
+        yellowButton?.addTarget(self, action: #selector(checkPriorityButton(_:)), for: .touchUpInside)
+        redButton?.addTarget(self, action: #selector(checkPriorityButton(_:)), for: .touchUpInside)
+        
+        if task?.priority == 1 {
+            greenButton?.isSelected = true
+        } else if task?.priority == 2 {
+            yellowButton?.isSelected = true
+        } else if task?.priority == 3 {
+            redButton?.isSelected = true
+        }
+        
         //        bottomView.listPicker.dataSource = self
         //        bottomView.listPicker.delegate = self
         self.view = bottomView
         
     }
     
-    override func viewDidLoad() {
+    @objc func saveButton(_ sender: UIButton) {
         
-//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardAppeared(notification:)), name: .expandModal, object: nil)
-    }
-//    @objc func keyboardAppeared(notification: NSNotification){
-//        // expand view
-//    }
-    
-    @objc func saveButton() {
-        // save task
-        // dismiss modal
-        self.dismiss(animated: true) {
+        guard let task = task else {return}
+        
+        // if user doesn't write anything we attribute task info for it, so it doesn't save an empty string
+        if taskNameTextField?.text == "" {
+            taskNameTextField?.text = task.name
+        }
+
+        if tagTextField?.text == "" {
+            tagTextField?.text = task.tags
+        }
+
+        // updating the task
+        self.homeModel.updateTask(task: task, name: taskNameTextField?.text ?? "", finishedAt: Date(), lastMovedAt: task.lastMovedAt ?? Date(), priority: Int64(priority ?? 0), status: task.status, tags: tagTextField?.text ?? "")
+      
+        dismiss(animated: true) {
+            self.delegate?.modalDismissed()
         }
     }
     
-    @objc func cancelButton() {
-        // dismiss modal
+    @objc func cancelButton(_ sender: UIButton) {
+        dismiss(animated: true) {
+            self.delegate?.modalDismissed()
+        }
     }
     
+    func checkIsSelected(button: UIButton) -> Bool {
+        if button.isSelected {
+            button.isSelected = false
+            
+        } else {
+            button.isSelected = true
+        }
+        return button.isSelected
+    }
+    
+    @objc func checkPriorityButton(_ sender: UIButton) {
+        
+        if sender == greenButton {
+            if checkIsSelected(button: greenButton ?? UIButton()) {
+                redButton?.isSelected = false
+                yellowButton?.isSelected = false
+                priority = 1
+            }
+            
+        } else if sender == redButton {
+            if checkIsSelected(button: redButton ?? UIButton()) {
+                greenButton?.isSelected = false
+                yellowButton?.isSelected = false
+                priority = 3
+            }
+            
+        } else {
+            if checkIsSelected(button: yellowButton ?? UIButton()) {
+                greenButton?.isSelected = false
+                redButton?.isSelected = false
+                priority = 2
+            }
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        //        NotificationCenter.default.addObserver(self, selector: #selector(keyboardAppeared(notification:)), name: .expandModal, object: nil)
+    }
+    //    @objc func keyboardAppeared(notification: NSNotification){
+    //        // expand view
+    //    }
 }
 
 extension BottomSheetViewController: UIPickerViewDataSource,UIPickerViewDelegate {
